d7e7628b41419dcd88f35fb715580f48c246f838
zfelipesilveira <zfelipesilveira@gmail.com>
2020-06-30 01:35:05 -0300

    Criação da classe que lê os diferentes tipos de txt

--- src/TratamentoArquivos/LeituraDeTextos.java
+++ src/TratamentoArquivos/LeituraDeTextos.java
@@ -1,4 +1,104 @@
 package TratamentoArquivos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class LeituraDeTextos {
+
+    public void leRegistrosTextoClientes() {
+        Path path = Paths.get("teste2.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                // separador: ;
+                Scanner sc = new Scanner(linha).useDelimiter(";");
+                System.out.println(sc.next()+"; "+sc.next()+"; "+ sc.next());
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+    }
+
+    public void leRegistrosTextoCategorias() {
+        Path path = Paths.get("teste2.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                // separador: ;
+                Scanner sc = new Scanner(linha).useDelimiter(";");
+                System.out.println(sc.next()+"; "+sc.next()+"; "+ sc.next());
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+    }
+
+    public void leRegistrosTextoMarcas() {
+        Path path = Paths.get("teste2.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                // separador: ;
+                Scanner sc = new Scanner(linha).useDelimiter(";");
+                System.out.println(sc.next()+"; "+sc.next()+"; "+ sc.next());
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+    }
+
+    public void leRegistrosTextoModelos() {
+        Path path = Paths.get("teste2.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                // separador: ;
+                Scanner sc = new Scanner(linha).useDelimiter(";");
+                System.out.println(sc.next()+"; "+sc.next()+"; "+ sc.next());
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+    }
+
+    public void leRegistrosTextoAutomoveis() {
+        Path path = Paths.get("teste2.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                // separador: ;
+                Scanner sc = new Scanner(linha).useDelimiter(";");
+                System.out.println(sc.next()+"; "+sc.next()+"; "+ sc.next());
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+    }
+
+    public void leRegistrosTextoLocacoes() {
+        Path path = Paths.get("teste2.txt");
+        try (BufferedReader br = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String linha = null;
+            while ((linha = br.readLine()) != null) {
+                // separador: ;
+                Scanner sc = new Scanner(linha).useDelimiter(";");
+                System.out.println(sc.next()+"; "+sc.next()+"; "+ sc.next());
+            }
+        }
+        catch (IOException e) {
+            System.err.format("Erro de E/S: %s%n", e);
+        }
+    }
+
+
 }
